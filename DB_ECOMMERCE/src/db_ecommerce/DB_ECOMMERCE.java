a84223bc9bd14074e92f6ae1ebd982e1e2dd3e12
nauvalhariezy <66213465+nauvalhariezy@users.noreply.github.com>
2022-06-24 16:15:04 +0700

    File NetBeans (DB_ECOMMERCE)
    Final Build JAVA (NETBEANS) 
    Program Proyek Akhir Praktikum Basis Data 2022
    Mifta Nauval Harizy NPM 06.2020.1.90505

---
+++ DB_ECOMMERCE/src/db_ecommerce/DB_ECOMMERCE.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package db_ecommerce;
+import GUI.*;
+/**
+ *
+ * @author Nauvalhariezy
+ */
+public class DB_ECOMMERCE {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Main_Interface main = new Main_Interface();
+        main.setVisible(true);
+    }
+    
+}
